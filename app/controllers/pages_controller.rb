dc8c840f29edd566b4d4d4ee6873becc05643283
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-05 11:08:26 -0500

    deleted grandma.html, random year added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def grandma
     if params[:user_input].upcase == params[:user_input]
-      @grandma = 'NO.  NOT SINCE 1963!'
+      @grandma = 'NO.  NOT SINCE ' + rand(1940..1965).to_s + '!'
     else
       @grandma = 'WHAT?  SPEAK UP, SONNY!'
     end
